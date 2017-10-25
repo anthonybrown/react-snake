d41d780c5b6f3f525bd8e3977f5c0b5678c81ee6
Tony Brown <tony@tonybrown.me>
2017-10-25 08:37:12 -0400

    use create react app to bootstrap application

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import cs from 'classnames';
+import './App.css';
+
+const GRID_SIZE = 35;
+const GRID = [];
+
+for (let i = 0; i <= GRID_SIZE; i++) {
+  GRID.push(i);
+}
+
+const isBorder = (x, y) =>
+	x === 0 || y === 0 || x === GRID_SIZE || y === GRID_SIZE
+
+const getCellCs = (x, y) =>
+	cs('grid-cell', {
+		'grid-cell-border': isBorder(x, y)
+	});
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {}
+	}
+
+  render() {
+    return (
+      <div className="app">
+				<h1>Snake</h1>
+				<Grid />
+      </div>
+    );
+  }
+}
+
+const Grid = () =>
+  <div>
+		{GRID.map(y =>
+			<Row
+				y={y}
+				key={y}
+			/>
+		)}
+	</div>
+
+
+const Row = ({ y }) =>
+	<div className="grid-row">
+	  {GRID.map(x =>
+			<Cell
+				x={x}
+				y={y}
+				key={x}
+			/>
+		)}
+	</div>
+
+const Cell = ({ x, y }) =>
+	<div className={getCellCs(x, y)}></div>
+
+
+export default App;
